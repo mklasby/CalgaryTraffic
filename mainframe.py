5f1de047aeb8e16407e7cba37eb52a0b96cd2cf5
Mike Lasby <mklasby@ucalgary.ca>
2020-07-17 11:07:55 -0600

    database functionality push pull working

--- mainframe.py
+++ mainframe.py
@@ -7,7 +7,7 @@ from getpass import getpass
 
 class Mainframe():
     '''
-    A collection of pd.Dataframe objects
+    A collection of pd.Dataframe objects used for interacting with database
     '''
 
     def __init__(self, db_name="default"):
@@ -42,16 +42,14 @@ class Mainframe():
             self.clustercluster = MongoClient('mongodb+srv://mklasby:' + pw
                                               + '@cluster0.hkive.mongodb.net/<dbname>?retryWrites=true&w=majority')
         # creates db named db_name within my cluster
-        db = self.cluster[self.db_name]
-        db_on = True
+        self.db = self.cluster[self.db_name]
 
     def push_data(self):
         if not self.cluster:  # if cluster has not be started
             # defaults to looking at local cluster
             self.init_db()
         for data_frame in self.data_frames:
-            db = self.cluster[self.db_name]
-            collection = db[data_frame.name]
+            collection = self.db[data_frame.name]
             data_frame.reset_index(inplace=True)
             data_dict = data_frame.to_dict("records")
             collection.insert_many(data_dict)
@@ -62,8 +60,22 @@ class Mainframe():
         print("Dropping DB named: " + self.db_name)
         self.cluster.drop_database(self.db_name)
 
+    # TODO: move follow to a pandas type class?
+
+    def get_collection(self, name, query={}):
+        print(
+            f"Getting collection {name} from {self.db_name} using query {query}")
+        cursor = self.db[name].find(query)
+        collection = pd.DataFrame(list(cursor))
+        return collection
+
+    def get_series(self, collection, query={}):
+        print(f"Getting series from {collection.name} using query {query}")
+        return collection.get("START_DT")
+
 
 def main():
+    incidents = "Traffic_Incidents"
     mainframe = Mainframe("calgaryTraffic")
     mainframe.drop_db()
     mainframe.load_data("Traffic_Incidents.csv")
@@ -75,6 +87,27 @@ def main():
 
     mainframe.init_db()
     mainframe.push_data()
+
+    incidents_frame = mainframe.get_collection(incidents)
+    print(incidents_frame)
+
+    def find_worst_inter(year):
+        data = mainframe.get_collection(
+            "Traffic_Incidents", {'START_DT': {'$regex': year}})
+
+        info = data.get("INCIDENT INFO")
+        hist = {}
+        for index, content in info.items():
+            hist[content] = hist.get(content, 0) + 1
+
+        sorted_hist = {k: v for k, v in sorted(
+            hist.items(), key=lambda x: x[1], reverse=True)}
+        print(list(sorted_hist.items())[:10])
+        print('\n')
+
+    find_worst_inter("2016")
+    find_worst_inter("2017")
+    find_worst_inter("2018")
 
     # TrafficFlow2016_OpenData.csv
     # 2017_Traffic_Volume_Flow.csv
