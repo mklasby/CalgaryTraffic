6f186ba05420d07772ad242c6379a1bc4a26e96e
Mike Lasby <mklasby@ucalgary.ca>
2020-07-17 09:49:27 -0600

    Polish to mainframe.py

--- mainframe.py
+++ mainframe.py
@@ -4,50 +4,25 @@ from pymongo import MongoClient
 import pandas as pd
 from getpass import getpass
 
-# Following is for connecting to my cloud cluster
-# TODO: Hide PW
-# pw = "Hikehigh00!mongodb"  # getpass("Enter DB Password")
-# cluster = MongoClient('mongodb+srv://mklasby:' + pw
-#                       + '@cluster0.hkive.mongodb.net/<dbname>?retryWrites=true&w=majority')
-
-# local cluster
-
-
-def mongo_test():
-    cluster = MongoClient(
-        'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
-    db = cluster["calgaryTraffic"]
-    collection = db["test"]
-
-    post1 = {"_id": 0, "name": "rachel", "score": 5}
-    post2 = {"_id": 1, "name": "mike", "score": 5}
-
-    collection.insert_many([post1, post2])
-
-    results = collection.find({"name": "mike"})
-
-    for items in results:
-        print(items)
-        print(items["_id"])
-
-    results = collection.update_one({"_id": 0, }, {"$set": {"name": "josh"}})
-
 
 class Mainframe():
     '''
     A collection of pd.Dataframe objects
     '''
 
-    def __init__(self):
+    def __init__(self, db_name="default"):
         self.data_frames = []
+        self.db_name = db_name
+        self.cluster = False
 
     def load_data(self, name):
         data_frame = pd.read_csv(name)
         data_frame.name = name.split('.')[0]
-        print(data_frame.name)
+        print("Loading: "+data_frame.name)
         self.data_frames.append(data_frame)
 
     def display_CLI(self):
+        '''Prints relevent information about all loaded dataframes'''
         for data in self.data_frames:
             print(type(data))
             print(len(data))
@@ -57,31 +32,49 @@ class Mainframe():
     def init_db(self, local=True):
         if local:
             # local cluster
-            cluster = MongoClient(
+            print("Connecting to local cluster")
+            self.cluster = MongoClient(
                 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false')
         else:
             # cloud cluster
+            print("Conntect to Mongo Atlas cluster")
             pw = getpass("Enter DB Password >>> ")
-            cluster = MongoClient('mongodb+srv://mklasby:' + pw
-                                  + '@cluster0.hkive.mongodb.net/<dbname>?retryWrites=true&w=majority')
+            self.clustercluster = MongoClient('mongodb+srv://mklasby:' + pw
+                                              + '@cluster0.hkive.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        # creates db named db_name within my cluster
+        db = self.cluster[self.db_name]
+        db_on = True
 
-        db = cluster["calgaryTraffic"]
+    def push_data(self):
+        if not self.cluster:  # if cluster has not be started
+            # defaults to looking at local cluster
+            self.init_db()
         for data_frame in self.data_frames:
+            db = self.cluster[self.db_name]
             collection = db[data_frame.name]
             data_frame.reset_index(inplace=True)
             data_dict = data_frame.to_dict("records")
             collection.insert_many(data_dict)
 
+    def drop_db(self, db_name="default"):
+        if not self.cluster:
+            self.init_db()
+        print("Dropping DB named: " + self.db_name)
+        self.cluster.drop_database(self.db_name)
+
 
 def main():
-    mainframe = Mainframe()
+    mainframe = Mainframe("calgaryTraffic")
+    mainframe.drop_db()
     mainframe.load_data("Traffic_Incidents.csv")
     mainframe.load_data("TrafficFlow2016_OpenData.csv")
     mainframe.load_data("2017_Traffic_Volume_Flow.csv")
     mainframe.load_data("Traffic_Volumes_for_2018.csv")
 
     mainframe.display_CLI()
+
     mainframe.init_db()
+    mainframe.push_data()
 
     # TrafficFlow2016_OpenData.csv
     # 2017_Traffic_Volume_Flow.csv
